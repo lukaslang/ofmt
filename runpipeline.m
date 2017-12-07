3a9b079782748a6714e9e5b18e35d2418961fcde
Lukas Lang <ll542@cam.ac.uk>
2017-12-07 17:13:39 +0000

    Moved check.

--- runpipeline.m
+++ runpipeline.m
@@ -36,21 +36,7 @@ fprintf('Starting analysis of folder: %s\n', datapath);
 fprintf('Outpus folder set to: %s\n', resultfolder);
 
 % Check if segmentation map is available for each dataset.
-for k=1:length(groups)
-    groupname = groups(k).name;
-    % Run through all datasets.
-    y = dir(fullfile(datapath, groupname));
-    y = y(~cellfun(@(x) strcmp(x, '.') || strcmp(x, '..'), {y.name}));
-    datasets = y([y.isdir]);
-    for l=1:length(datasets)
-        dataset = datasets(l).name;
-        datafolder = fullfile(datapath, groupname, dataset);
-
-        if(~exist(fullfile(datafolder, 'images', 'segmentationMap.png'), 'file'))
-            warning('Segmentation map missing for dataset: %s\n', datafolder);
-        end
-    end
-end
+checkSegmentationMap(groups);
 
 % Run through all groups.
 for k=1:length(groups)
@@ -83,4 +69,24 @@ for k=1:length(groups)
     end
 end
 
-% TODO: Add group/combined analysis.
+% TODO: Add group/combined analysis
+
+
+function checkSegmentationMap(groups)
+% CHECKSEGMENTATIONMAP Runs a quick check if for every dataset a
+% segmentation exists. Fails with error.
+	for k=1:length(groups)
+        groupname = groups(k).name;
+        % Run through all datasets.
+        y = dir(fullfile(datapath, groupname));
+        y = y(~cellfun(@(x) strcmp(x, '.') || strcmp(x, '..'), {y.name}));
+        datasets = y([y.isdir]);
+        for l=1:length(datasets)
+            dataset = datasets(l).name;
+            datafolder = fullfile(datapath, groupname, dataset);
+            if(~exist(fullfile(datafolder, 'images', 'segmentationMap.png'), 'file'))
+                error('Segmentation map missing for dataset: %s\n', datafolder);
+            end
+        end
+    end
+end
