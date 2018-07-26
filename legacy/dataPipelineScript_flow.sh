858a86e8e29fee450f20727444670ff8920340ff
Lukas Lang <ll542@cam.ac.uk>
2018-07-26 10:25:52 +0100

    Changed matlab version in run script.

--- legacy/dataPipelineScript_flow.sh
+++ legacy/dataPipelineScript_flow.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup nice matlab-r2017b -nodisplay -nojvm -nosplash -nodesktop -r "try; run('dataPipelineScript_flow.m'); catch ME; disp(ME); disp(getReport(ME, 'extended', 'hyperlinks', 'off')); exit(1); end; exit(0);" > dataPipelineScript_flow.log 2>&1
+nohup nice matlab-r2016b -nodisplay -nojvm -nosplash -nodesktop -r "try; run('dataPipelineScript_flow.m'); catch ME; disp(ME); disp(getReport(ME, 'extended', 'hyperlinks', 'off')); exit(1); end; exit(0);" > dataPipelineScript_flow.log 2>&1
