e937009d96376a335e05f790dba8caf049984859
Lukas Lang <ll542@cam.ac.uk>
2018-07-26 10:45:24 +0100

    Updated script.

--- legacy/dataPipelineScript_flow.sh
+++ legacy/dataPipelineScript_flow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-env LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/local/cuda/lib64/libcudart.so /usr/local/cuda/lib64/libcusparse.so"
+env LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.21 /usr/local/cuda/lib64/libcudart.so /usr/local/cuda/lib64/libcusparse.so"
 nohup nice matlab-r2016b -nodisplay -nojvm -nosplash -nodesktop -r "try; run('dataPipelineScript_flow.m'); catch ME; disp(ME); disp(getReport(ME, 'extended', 'hyperlinks', 'off')); exit(1); end; exit(0);" > dataPipelineScript_flow.log 2>&1
