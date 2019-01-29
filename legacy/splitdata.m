98400e943433720a36734161bc9dcad002054d45
Lukas Lang <ll542@cam.ac.uk>
2019-01-29 09:47:20 +0000

    Moved files.

--- legacy/splitdata.m
+++ legacy/splitdata.m
@@ -1,5 +1,23 @@
+% Copyright 2017 Lukas Lang
+%
+% This file is part of OFMT.
+%
+%    OFMT is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    OFMT is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with OFMT.  If not, see <http://www.gnu.org/licenses/>.
+
 % This script takes a data folder and splits the data into smaller 
 % sequences e.g. of 50 frames.
+% This is required for the joint approach due to memory issues.
 clear;
 close all;
 clc;
